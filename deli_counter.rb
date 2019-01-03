61f89d79c377b6be4e4f5eb070360a4f16961f79
Lemrabt Boudadya <lboudadya@gmail.com>
2019-01-03 23:18:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def line(katz_deli)
+  new_array = []
+  if katz_deli.length == 0 
+     puts "The line is currently empty."
+   else 
+     
+     katz_deli.each_with_index do |name, index|
+       index += 1
+      new_array.push("#{index}. #{name}")
+      
+    end  
+    puts "The line is currently: " + new_array.join(" ")
+  end
+end
+
+def take_a_number(current_line, name)
+  katz_deli = []
+  number = 1 
+  current_line.each do |name|
+  katz_deli.push("Welcome, #{name}. You are number #{number} in line.")
+  number += 1 
+  end
+  puts ""
+end  
